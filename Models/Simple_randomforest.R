627b6531012ca26962e26be2d89129ac26bd64b3
Vishnu Unnikrishnan <optra7@gmail.com>
2020-02-15 08:01:26 +0530

    complete RandomForest

--- Models/Simple_randomforest.R
+++ Models/Simple_randomforest.R
@@ -2,31 +2,54 @@ library(randomForest)
 library(caret)
 
 
-air <- read.csv(file = "classification_good_haz.csv")
-air$PM25 <- as.numeric(air$PM25)
+air <- read.csv(file = "orginal.csv")
 str(air)
-
-air$PM25 <- as.factor(air$PM25)
-table(air$PM25)
-
 
 set.seed(1234)
 ind <- sample(2, nrow(air), replace = T, prob = c(.7, .3))
 training <- air[ind==1, ]
 test <- air[ind==2, ]
 
-
 set.seed(222)
-rf <- randomForest(PM25~., data = training)
+rf <- randomForest(PM25~., data = training,
+                   ntree = 300,
+                   mtry = 12,
+                   importance = T,
+                   proximity = T)
 print(rf)
 attributes(rf)
 
 
-p1 <- predict(rf, training)
-confusionMatrix(p1, training$PM25)
+p1 <- predict(rf, test)
+rmse(log(test$PM25),log(p1))
 
-  
+pre <- Prediction_3
+act <- testing$PM25
+t1 <- cbind(pre,act)
+view(t1)
 
-p2 <- predict(rf, test)
-confusionMatrix(p1, training$PM25)
+plot(rf)
+ 
+# Tuning
+t <- tuneRF(training[, -11], training[, 11],
+       stepFactor = 0.5,
+       plot = TRUE,
+       ntreeTry = 300,
+       trace = T,
+       improve = 0.05)
 
+#Histogram
+hist(treesize(rf),
+     main = "No of nodes for the trees",
+     col = "green")
+ 
+#variable Importence
+
+varImpPlot(rf,
+           sort = T,
+           n.var = 10,
+           main = "Top 10")
+
+importance(rf)
+# varUsed(rf)
+# # MDSplot(rf, t1)
