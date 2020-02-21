c2e9fc3eeb6e4b7bee10f6d02cf07146d3b0ebf5
Vishnu Unnikrishnan <optra7@gmail.com>
2020-02-21 11:41:35 +0530

    new

--- Models/Simple_randomforest.R
+++ Models/Simple_randomforest.R
@@ -14,8 +14,9 @@ test <- air[ind==2, ]
 #create model
 set.seed(222)
 rf <- randomForest(PM25~., data = training,
-                   mtry =12,
-                   ntree = 300)
+                   mtry =10,
+                   ntree = 500,
+                   proximity=T)
 #print model
 print(rf)
 attributes(rf)
@@ -32,7 +33,7 @@ act <- test$PM25
 t1 <- cbind(pre,act)
 view(t1)
 
-plot(rf)
+plot(rf, log="y")
  
 # Tuning
 t <- tuneRF(training[, -11], training[, 11],
@@ -57,3 +58,31 @@ varImpPlot(rf,
 importance(rf)
 # varUsed(rf)
 # # MDSplot(rf, t1)
+
+
+oob.err=double(13)
+test.err=double(13)
+
+#mtry is no of Variables randomly chosen at each split
+for(mtry in 1:13) 
+{
+        rf=randomForest(medv ~ . , data = Boston , subset = train,mtry=mtry,ntree=400) 
+        oob.err[mtry] = rf$mse[400] #Error of all Trees fitted
+        
+        pred<-predict(rf,Boston[-train,]) #Predictions on Test Set for each Tree
+        test.err[mtry]= with(Boston[-train,], mean( (medv - pred)^2)) #Mean Squared Test Error
+        
+        cat(mtry," ") #printing the output to the console
+        
+}
+## 1  2  3  4  5  6  7  8  9  10  11  12  13
+
+
+
+
+matplot(1:10 , cbind(oob.err,test.err), pch=19 , col=c("red","blue"),type="b",ylab="Mean Squared Error",xlab="Number of Predictors Considered at each Split")
+legend("topright",legend=c("Out of Bag Error","Test Error"),pch=19, col=c("red","blue"))
+
+
+
+obb.err
