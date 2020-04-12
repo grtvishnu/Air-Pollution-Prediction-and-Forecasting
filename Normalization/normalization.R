1f231437767d8cdebe7fc8d9936a3ab32ae689ae
Vishnu Unnikrishnan <optra7@gmail.com>
2020-04-12 20:07:09 +0530

    norm organized

--- Normalization/normalization.R
+++ Normalization/normalization.R
@@ -2,23 +2,25 @@ library(tidyverse)
 library(psycho)
 library(ggplot2)
 
+# Read
 air <- read_csv("data.csv")
 str(air)
 
+# Summary
 air %>% 
   select(PM25, AQI) %>% 
   summary()
 
+# Histogram pm25
 ggplot(data = air, aes(air$PM25))+
   geom_histogram()
 
 hist(air$PM25, col = 'red')
 hist(air$AQI, col = 'red')
 
+# Normalization by z score
 m <- colMeans(air)
 s <- apply(air, 2, sd)
 z_air <- scale(air, center = m, scale = s)
-
 z_air <- as.tibble(z_air)
-
 head(z_air)
