0775e7e7fdd9f877580567b1d46a0ba1fa78d560
Vishnu Unnikrishnan <optra7@gmail.com>
2020-04-12 19:22:05 +0530

    Catboost organized

--- Models/catboost.R
+++ Models/catboost.R
@@ -1,33 +1,29 @@
-# install.packages('devtools')
-# library(devtools)
-# devtools::install_github('catboost/catboost', subdir = 'catboost/R-package')
-# install.packages("catboost")
+#Libraries
 library(catboost)
 library(caret)
 library(dplyr)
 library(plotly)
 library(tidyverse)
+library(MLmetrics)
+# Read data
 air <- read.csv(file = "data.csv")
-# air <- air[,2:11]
 str(air)
-# air$PM25 <- as.numeric(air$PM25)
-# labels <- train$PM25
-
+#Split data
 set.seed(1234)
 ind <- sample(2, nrow(air), replace = T, prob = c(0.8, 0.2))
 train <- air[ind == 1, ]
 test <- air[ind == 2, ]
-
+# Create Label
 y_train <- unlist(train[c("PM25")])
 X_train <- train %>% select(-PM25)
 y_valid <- unlist(test[c("PM25")])
 X_valid <- test %>% select(-PM25)
 
-
+# create train pool and test pool
 train_pool <- catboost.load_pool(data = X_train, label = y_train)
 test_pool <- catboost.load_pool(data = X_valid, label = y_valid)
 
-
+# model
 params <- list(
   iterations = 1500,
   learning_rate = 0.01,
@@ -41,26 +37,20 @@ params <- list(
   use_best_model = TRUE
 )
 
-
-
-
+# compile
 model <- catboost.train(learn_pool = train_pool, params = params)
 
+#Summary
 catboost.get_model_params(model)
-# save(model, file = "catboot.rda")
-# predict
+
+# model prediction
 y_pred <- catboost.predict(model, test_pool)
 postResample(y_pred, test$PM25)
 
 rmse(log(test$PM25), log(y_pred))
 RMSE(y_pred, test$PM25, na.rm = T)
-# saveRDS(model, "catboost_model_chennai.RDS")
-
-plot(model)
-
 
 # feature importence
-
 importance <- catboost.get_feature_importance(model,
                                               pool = NULL,
                                               type = "FeatureImportance",
@@ -69,6 +59,8 @@ importance <- catboost.get_feature_importance(model,
 
 print(importance)
 
+
+# plot importance
 atta <- c("Temperature", "Windspeed", "Pressure", "NO2", "Rainfall", "PM10", "AQI")
 impo<-cbind(atta, importance)
 impo <- as.tibble(impo)
