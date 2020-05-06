ddde1ae89bf39bbaf3c317ac78539db9d4d7fd5a
Vishnu Unnikrishnan <optra7@gmail.com>
2020-05-06 12:48:22 +0530

    create catvariable

---
+++ Data Cleaning/to_cat.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+air <- read_csv("data.csv")
+
+AQI <- air$AQI
+
+x = c(0,50,100,150,200,300,Inf) 
+
+air$AQI <- cut(AQI,x, 6, labels = c("Good", "Moderate","Unhealthy for sensitive groups",
+                       "Unhealthy","Very Unhealthy","Hazardous"), )
+
+write_csv(air,"data_cat.csv")
