b5f917d98689cd21af4c580394027a08d3339282
Vishnu Unnikrishnan <optra7@gmail.com>
2020-06-13 18:05:02 +0530

    Update clean.R

--- airapi_model/clean.R
+++ airapi_model/clean.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(caret)
-
+library(Hmisc)
+library(PerformanceAnalytics)
+library(corrplot)
+library(ccgarch)
 
 # read data -----------------------------------------------------------------------------------
 
@@ -9,12 +12,46 @@ kollam <- read_csv("kollam.csv")
 kochi <- read_csv("kochi.csv")
 kannur <- read_csv("kannur.csv")
 
+# DataPreprocessing ---------------------------------------------------------------------------
+
+
 head(kozhi)
-
 kozhi<- kozhi[ ,-1]
-
-
 org_kozhi<- kozhi %>% 
   spread(parameter,value)
 
-rm(kozhi)
+# rm(kozhi)
+# kozhi <- org_kozhi
+# rm(org_kozhi)
+
+kozhi <- kozhi %>% 
+  select(-local,-latitude,-longitude)
+kozhi
+
+# Correlation ---------------------------------------------------------------------------------
+
+
+res<- kozhi %>% 
+  select(co:so2) %>% 
+  cor()
+
+chart.Correlation(res, histogram = T, pch = 19)
+corrplot(res, method="circle")
+corrplot(res, method="pie")
+corrplot(res, method="color")
+corrplot(res, method="number")
+corrplot(res, type="upper")
+corrplot(res, type="upper", order="hclust")
+palette = colorRampPalette(c("green", "white", "red")) (20)
+heatmap(x = res, col = palette, symm = TRUE)
+
+
+# Outlier treatment ---------------------------------------------------------------------------
+
+summary(kozhi)
+boxplot(kozhi$co)
+boxplot(kozhi$o3)
+boxplot(kozhi$so2)
+boxplot(kozhi$no2)
+boxplot(kozhi$pm25)
+boxplot(kozhi$pm10)
