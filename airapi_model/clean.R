ff4610b77e2252a24ee20e4cbbb2b6d0ee921f97
Vishnu Unnikrishnan <optra7@gmail.com>
2020-06-13 18:55:08 +0530

    Update clean.R

--- airapi_model/clean.R
+++ airapi_model/clean.R
@@ -55,3 +55,55 @@ boxplot(kozhi$so2)
 boxplot(kozhi$no2)
 boxplot(kozhi$pm25)
 boxplot(kozhi$pm10)
+
+summary(kozhi$co)
+summary(kozhi$no2)
+summary(kozhi$o3)
+summary(kozhi$pm10)
+summary(kozhi$pm25)
+summary(kozhi$so2)
+
+#winsering method for treating outliers
+
+#co
+bench <- 510 + 1.5*IQR(kozhi$co)
+kozhi<- kozhi %>% 
+  mutate(co=replace(co, co>1023.75,1023.75)) %>% 
+  mutate(co=replace(co, co<=0,50))
+summary(kozhi$co)
+
+#o3
+bench <- 12.232 + 1.5*IQR(kozhi$o3)
+kozhi<- kozhi %>% 
+  mutate(o3=replace(o3, o3>bench,bench))
+summary(kozhi$o3)
+
+#so2
+bench <- 3.130 + 1.5*IQR(kozhi$so2)
+kozhi<- kozhi %>% 
+  mutate(so2=replace(so2, so2>bench,bench))
+summary(kozhi$so2)
+boxplot(kozhi$so2)
+
+#no2
+bench <- 11.20 + 1.5*IQR(kozhi$no2)
+kozhi<- kozhi %>% 
+  mutate(no2=replace(no2, no2>bench,bench))
+summary(kozhi$no2)
+boxplot(kozhi$no2)
+
+#pm25
+bench <- 33.00 + 1.5*IQR(kozhi$pm25)
+kozhi<- kozhi %>% 
+  mutate(pm25=replace(pm25, pm25>bench,bench))
+summary(kozhi$pm25)
+boxplot(kozhi$pm25)
+
+#pm10
+bench <- 54.00 + 1.5*IQR(kozhi$pm10)
+kozhi<- kozhi %>% 
+  mutate(pm10=replace(pm10, pm10>bench,bench))
+summary(kozhi$pm10)
+boxplot(kozhi$pm10)
+
+rm(a,res,bench,bench1,palette)
