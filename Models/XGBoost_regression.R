62ec51f403c1bfdfedf728e61a52fe8b50e45c6b
grtvishnu <optra7@gmail.com>
2020-07-29 02:02:34 +0530

    Update XGBoost_regression.R

--- Models/XGBoost_regression.R
+++ Models/XGBoost_regression.R
@@ -23,28 +23,28 @@ dtrain <- xgb.DMatrix(label = labels, data = as.matrix(train))
 dtest <- xgb.DMatrix(label = ts_labels, data = as.matrix(test))
 
 # Model
-set.seed(123)
-xgbFit <- xgboost(
-  data = dtrain,
-  nfold = 5, label = labels,
-  nrounds = 2200, verbose = T, objective = "reg:linear",
-  eval_metric = "rmse", nthread = 8, eta = 0.01, gamma = 0.0468,
-  max_depth = 6, min_child_weight = 1.7817,
-  subsample = 0.5213,
-  colsample_bytree = 0.4603
-)
+# set.seed(123)
+# xgbFit <- xgboost(
+#   data = dtrain,
+#   nfold = 5, label = labels,
+#   nrounds = 2200, verbose = T, objective = "reg:linear",
+#   eval_metric = "rmse", nthread = 8, eta = 0.01, gamma = 0.0468,
+#   max_depth = 6, min_child_weight = 1.7817,
+#   subsample = 0.5213,
+#   colsample_bytree = 0.4603
+# )
 
-# xgb_new <- xgboost(data = dtrain,
-#                label = labels,
-#                trees = 1000,
-#                mtry = 8,
-#                min_n = 10,
-#                tree_depth = 3,
-#                learn_rate= 	0.00644471,
-#                loss_reduction = 0.000002150994,
-#                sample_size = 0.4017052,
-#                eval_metric ="rmse",
-#                nrounds = 1000)
+xgb_new <- xgboost(data = dtrain,
+               label = labels,
+               trees = 1000,
+               mtry = 8,
+               min_n = 10,
+               tree_depth = 3,
+               learn_rate= 	0.00644471,
+               loss_reduction = 0.000002150994,
+               sample_size = 0.4017052,
+               eval_metric ="rmse",
+               nrounds = 1000)
 
 # predict
 print(xgb_new)
